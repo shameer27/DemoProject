38c78cdf11b36104500bc2bd6288f4209f19853a
shameer <shameershams27@gmail.com>
2019-06-16 16:30:25 +0530

    Demo Finished

--- client/cust.js
+++ client/cust.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp",['ngRoute'])
+var app = angular.module("myApp", ['ngRoute'])
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/home', {
@@ -6,8 +6,16 @@ app.config(function ($routeProvider) {
             controller: 'myCtrl'
         })
         .when('/admin', {
+            templateUrl: 'admlog.html',
+            controller: 'myCtrl1'
+        })
+        .when('/admindashboard', {
             templateUrl: 'admin.html',
             controller: 'myCtrl4'
+        })
+        .when('/userdashboard', {
+            templateUrl: 'userdashboard.html',
+            controller: 'myCtrl5'
         })
         .when('/login', {
             templateUrl: 'log.html',
@@ -21,35 +29,143 @@ app.config(function ($routeProvider) {
             redirectTo: '/home'
         });
 });
-app.controller("myCtrl3",function($scope,$http){
-    $scope.ClickMe=function(){
-            
-            var values = {
-                      mail:$scope.mail,
-                      phno:$scope.phno,
-                      pwd:$scope.pwd,
-            }
-            $http.post('http://localhost:2500/reg', values).then(function (response) {                
-                    if(response.data.code == '0'){ 
-                        swal("Good job!", "Register Successfully!", "success");
-                    } else {
-                        swal("Sorry", "Failed", "failed");
-                    }
-                });
+app.controller("myCtrl3", function ($scope, $http, $location) {
+    $scope.ClickMe = function () {
+
+        var values = {
+            mail: $scope.mail,
+            phno: $scope.phno,
+            pwd: $scope.pwd,
         }
+        $http.post('http://localhost:2500/reg', values).then(function (response) {
+            if (response.data.code == '0') {
+                swal("Good job!", "Register Successfully!", "success");
+            } else {
+                swal("Sorry", "Failed", "failed");
+            }
+        });
+    }
+});
+
+app.controller("myCtrl1", function ($scope, $http, $location) {
+    $scope.adminloginfun = function () {
+        console.log("admin login")
+        var values = {
+            mail: $scope.mail,
+            pwd: $scope.pwd,
+            adminflag: true
+        }
+        console.log(values, "login function");
+        $http.post('http://localhost:2500/login', values).then(function (response) {
+            console.log(response.data)
+            if (response.data.status == '0') {
+                if (response.data.data.adminflag == true) {
+                    swal("Good job!", "login  Successfully!", "success");
+                    $location.path("/admindashboard");
+                } else {
+                    swal("Sorry!", "you dont have admin previlage login ", "failed");
+                }
+            } else {
+                swal("Sorry", "Failed", "failed");
+            }
+        });
+    }
+});
+app.controller("myCtrl2", function ($scope, $http, $location) {
+    $scope.loginfun = function () {
+        var values = {
+            mail: $scope.mail,
+            pwd: $scope.pwd,
+        }
+        console.log(values, "login function");
+        $http.post('http://localhost:2500/login', values).then(function (response) {
+            console.log(response.data.data.mail)
+            if (response.data.status == '0') {
+                if (response.data.message == "please verify your account") {
+                    swal("Sorry!", response.data.message, "failed");
+                } else {
+                    swal("Good job!", "login  Successfully!", "success");
+                    localStorage.setItem("usermail", response.data.data.mail)
+                    $location.path("/userdashboard");
+                }
+            } else {
+                swal("Sorry", "Failed", "failed");
+            }
+        });
+    }
 });
 app.controller('myCtrl4', function ($scope, $http) {
-$scope.getdataasjson = function () {
-    $http.get('http://localhost:2500/getdata').success(function (response) {
-        if (response.status == '1') {
-            console.log(response, "response")
-            $scope.demo = response.data;
+    $scope.getdataasjson = function () {
+        $http.get('http://localhost:2500/getdata').success(function (response) {
+            if (response.status == '1') {
+                console.log(response, "response")
+                $scope.demo = response.data;
+            }
+        });
+    }
+    $scope.save = function (x, y) {
+        var demo = {
+            name: x.name,
+            mail: x.mail,
+            phno: x.phno,
+            pwd: x.pwd,
+            dob: x.dob,
+            status: x.status,
+            id: y
         }
-    });
-}
-// });
-// app.controller('myCtrl1', function ($scope) {
-//     $scope.clickHere = function () {
-//         location.href="admin.html"
-//       } 
+        console.log(demo);
+        $http.put('http://localhost:2500/upddata/', demo).success(function (response) {
+            if (response.status == '1') {
+                swal("Good job!", "Updated Successfully!", "success");
+                $scope.getdataasjson();
+            }
+        });
+    }
+    $scope.deletedata = function (id) {
+        $http.delete('http://localhost:2500/deldata/' + id).success(function (response) {
+            if (response.status == '1') {
+                swal("Good job!", "Deleted Successfully!", "success");
+                $scope.getdataasjson();
+            }
+        });
+    }
+});
+app.controller('myCtrl5', function ($scope, $http, $location) {
+    $scope.getdata = function () {
+        var mail = localStorage.getItem("usermail")
+        console.log(mail)
+        $http.get('http://localhost:2500/getdatauser/'+mail).success(function (response) {
+            console.log(response.data)
+            if (response.status == '0') {
+                console.log(response.data, "response")
+                $scope.data = response.data;
+            }
+        });
+    }
+    $scope.save = function (data, y) {
+        var demo = {
+            name: data.name,
+            mail: data.mail,
+            phno: data.phno,
+            pwd: data.pwd,
+            dob: data.dob,
+            status: data.status,
+            id: y
+        }
+        console.log(demo);
+        $http.put('http://localhost:2500/userupddata/', demo).success(function (response) {
+            if (response.status == '1') {
+                swal("Good job!", "Updated Successfully!", "success");
+                $scope.getdataasjson();
+            }
+        });
+    }
+    $scope.deletedata = function (id) {
+        $http.delete('http://localhost:2500/deldata/' + id).success(function (response) {
+            if (response.status == '1') {
+                swal("Good job!", "Deleted Successfully!", "success");
+                $scope.getdataasjson();
+            }
+        });
+    }
 });
--- server/server.js
+++ server/server.js
@@ -9,8 +9,10 @@ var users = mongoose.model('test', {
     name: { type: String },
     mail: { type: String },
     phno: { type: String },
-    dob: { type: String},   
-    pwd: { type: String}
+    dob: { type: String },
+    status: { type: String, default: "inactive" },
+    adminflag: { type: Boolean, default: false },
+    pwd: { type: String }
 
 }); //Model declaration
 
@@ -23,18 +25,58 @@ app.all('*', function (req, res, next) {
 });
 app.post('/reg', function (req, res) {
     var datavalue = new users(req.body);
-    console.log("Data---",datavalue);
+    console.log("Data---", datavalue);
     //console.log(typeof datavalue, "datavalue")
     datavalue.save(function (err, data) { //Query To insert details to mongodb
         //console.log(data, "after saved");
         if (err) {
-            res.send({code:1})            
+            res.send({ code: 1 })
         }
         else {
             console.log(data);
-            res.send({code:0})
+            res.send({ code: 0 })
         }
     });
+});
+app.post('/login', function (req, res) {
+    var datavalue = req.body;
+
+    console.log("Data---", datavalue);
+    if (datavalue.adminflag) {
+        var query = {
+            mail: datavalue.mail,
+            pwd: datavalue.pwd,
+            adminflag: datavalue.adminflag,
+        }
+        users.findOne(query).exec(function (err, data) {
+            if (err) {
+                res.send({ status: "1", err: err });
+            } else {
+                console.log(data, "demo1 value")
+                res.send({ status: "0", data: data });
+            }
+        });
+
+    } else {
+        var query = {
+            mail: datavalue.mail,
+            pwd: datavalue.pwd,
+        }
+        users.findOne(query).exec(function (err, data) {
+            if (err) {
+                res.send({ status: "1", err: err });
+            } else {
+                console.log(data, "demo1 value")
+                if (data.status == "active") {
+                    res.send({ status: "0", data: data });
+                } else {
+                    res.send({ status: "0", message: "please verify your account", data: data })
+                }
+            }
+        });
+    }
+    //console.log(typeof datavalue, "datavalue")
+
 });
 app.get('/getdata', function (req, res) {
     users.find({}).exec(function (err, demo1) {
@@ -42,6 +84,69 @@ app.get('/getdata', function (req, res) {
 
         } else {
             res.send({ status: "1", data: demo1 });
+        }
+    });
+});
+app.get('/getdatauser/:mail', function (req, res) {
+    console.log(req.params.mail, "sd")
+    var query = {
+        mail: req.params.mail
+    }
+    users.findOne(query).exec(function (err, demo1) {
+        if (err) {
+
+        } else {
+            res.send({ status: "0", data: demo1 });
+        }
+    });
+});
+app.put('/userupddata', function (req, res) {
+    console.log(req.body)
+    var update = {
+        name: req.body.name,
+        mail: req.body.mail,
+        phno: req.body.phno,
+        pwd: req.body.pwd,
+        status: req.body.status,
+        dob: req.body.dob,
+    }
+    var datavalue = new users(req.body);
+    users.findOneAndUpdate({ 'mail': req.body.mail }, update).exec(function (err, demo1) {
+        if (err) {
+
+        } else {
+
+            res.send({ status: "1" });
+        }
+    });
+});
+app.put('/upddata', function (req, res) {
+    var update = {
+        name: req.body.name,
+        mail: req.body.mail,
+        phno: req.body.phno,
+        pwd: req.body.pwd,
+        status: req.body.status,
+        dob: req.body.dob,
+    }
+    var datavalue = new users(req.body);
+    users.findOneAndUpdate({ '_id': req.body.id }, update).exec(function (err, demo1) {
+        if (err) {
+
+        } else {
+
+            res.send({ status: "1" });
+        }
+    });
+});
+app.delete('/deldata/:id', function (req, res) {
+    console.log(req.params.id, "delete data")
+    users.findOne({ '_id': req.params.id }).exec(function (err, demo1) {
+        if (err) {
+
+        } else {
+            demo1.remove();
+            res.send({ status: "1" });
         }
     });
 });
